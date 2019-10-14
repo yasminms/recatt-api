582ee826a0808685a679453f18adf49c2cbaf6f7
Yasmin Moraes da Silva <yasminms46@gmail.com>
2019-10-14 09:32:22 -0300

    feat: adicionando estrutura do projeto

---
+++ src/main/java/br/com/recatt/RecattApplication.java
@@ -0,0 +1,13 @@
+package br.com.recatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecattApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecattApplication.class, args);
+    }
+
+}
